c16344d6a85f43dc3ccc4b03d92e9bb79f435d5b
Alec Clews <alecclews@gmail.com>
2014-12-08 16:00:09 +1100

    Title: Added support for ino Arduino build tool
    Detail:
     * New dir layout,
     * Ignore build directory

--- sketch/src/sketch.ino
+++ sketch/src/sketch.ino
@@ -1,6 +1,8 @@
 #define MAX_AXIS 3
-#define MAX_RECORDS 10
+#define MAX_RECORDS 4
 #define MAX_INDEX (MAX_RECORDS-1)
+#define DELAY 1000 // millisends
+#define POST_MOVE_DELAY 1000 // 1 seconds
 
 static const byte PIN_X_AXIS = A0;
 static const byte PIN_Y_AXIS = A1;
@@ -11,35 +13,32 @@ static int current = 0;
 
 void setup() {
   for (byte i = 0 ; i < MAX_AXIS; i++)
-      for (byte j = 0 ; i < MAX_RECORDS; i++)
+      for (byte j = 0 ; j < MAX_RECORDS; j++)
             rolling[i][j] = 0;
-            
+
   Serial.begin(38400);
 }
 
 void loop() {
-  Serial.print("Current = ");
-  Serial.println(current); 
+  delay(DELAY);
   int delta1 = 0;
   int delta2 = 0;
 
   for (byte i = 0; i < MAX_AXIS; i++) {
      int r =  analogRead(PIN_X_AXIS+i);
+     Serial.print("Read from Pin ");
+     Serial.print(r);
      rolling[i][current?current-1:MAX_INDEX] = abs(rolling[i][current?current-1:MAX_INDEX]-r);
      rolling[i][current] = r;
-     Serial.print(" Stored value ="); 
-     Serial.print(rolling[i][current]); 
-     Serial.print(" Delta1  = "); 
-     Serial.print(delta1+=rolling[i][current?current-1:MAX_INDEX]); 
-     Serial.print(" Delta2  = "); 
-     Serial.println(delta2+=rolling[i][current-1?current-2:MAX_INDEX]); 
+     delta1+=rolling[i][current?current-1:MAX_INDEX];  
+     delta2+=rolling[i][current-1?current-2:MAX_INDEX]; 
   }
-  
-  Serial.print("Total Delta1 = "); 
-  Serial.println(delta1/3); 
-  Serial.print("Total Delta2 = "); 
-  Serial.println(delta2/3); 
+
+  if ((delta2+delta1)/3 > 100) {
+     Serial.println(" Movement detected");
+     delay(POST_MOVE_DELAY);
+  }
+
   ++current %= 10;
-  delay(2000);
 }
 
